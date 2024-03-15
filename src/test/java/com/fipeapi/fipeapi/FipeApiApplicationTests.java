069fbeae0830aac708ba2a81967014f5fa667377
Waldir Júnior <waldirjunior.gpn@gmail.com>
2024-03-14 22:07:18 -0300

    primeiro commit

---
+++ src/test/java/com/fipeapi/fipeapi/FipeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fipeapi.fipeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FipeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
